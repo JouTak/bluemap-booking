4445c4f9f646d27c31f03b3a1f66da9eef56daf9
Denis K. Berman (EnderDissa) <enderdissa@gmail.com>
2026-06-13 04:08:47 +0300

    build: replace paperweight with paper-api, drop modrinth publishing, unify version

--- buildSrc/src/main/kotlin/paper-script.gradle.kts
+++ buildSrc/src/main/kotlin/paper-script.gradle.kts
@@ -1,28 +1,23 @@
 plugins {
     kotlin("jvm")
-    id("io.papermc.paperweight.userdev")
     id("xyz.jpenilla.run-paper")
 }
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.codemc.org/repository/maven-public/")
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("de.miraculixx:kpaper:1.2.1")
-    compileOnly("dev.jorel:commandapi-paper-core:11.0.0")
+    compileOnly("dev.jorel:commandapi-paper-core:11.1.0")
     implementation("dev.jorel:commandapi-kotlin-paper:11.1.0")
     implementation("dev.jorel:commandapi-kotlin-core:11.1.0")
     implementation("dev.jorel:commandapi-paper-shade:11.1.0")
-
-
-
 }
 
-tasks {
-    assemble {
-        dependsOn(reobfJar)
-    }
+tasks.runServer {
+    minecraftVersion("1.21.11")
 }
